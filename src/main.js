1f61579264a899f6d6b4045cae07a5d3578f39ac
TristanSch1 <tristan.sch57@gmail.com>
2022-03-30 15:48:34 +0200

    ✨ Adding vuex in the project

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
+import store from "./store/index.js";
+
 /* import the fontawesome core */
 import { library } from "@fortawesome/fontawesome-svg-core";
 
@@ -26,5 +28,6 @@ Vue.component("font-awesome-icon", FontAwesomeIcon);
 Vue.config.productionTip = false;
 
 new Vue({
+  store,
   render: (h) => h(App),
 }).$mount("#app");
